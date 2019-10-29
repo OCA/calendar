887c1e626b679fbf33a58bd166794980437b9287
sbejaoui <souheil.bejaoui@acsone.eu>
2019-10-29 10:54:33 +0100

    [12.0][ADD] - add a link between project tasks and calendar events

---
+++ calendar_event_link_base/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2019 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Calendar Event Link Base",
+    "summary": """
+        This module add an abstract model that provide an action to link
+        any model to calendar events""",
+    "version": "12.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "ACSONE SA/NV,"
+              "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/calendar",
+    "depends": ["calendar"],
+}
