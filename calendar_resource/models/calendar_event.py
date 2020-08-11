0b5ee0d5ef7857ffabcb59a2954b6500b1fd17c9
Radovan Skolnik <radovan@skolnik.info>
2020-08-11 22:40:47 +0200

    ..

--- calendar_resource/models/calendar_event.py
+++ calendar_resource/models/calendar_event.py
@@ -206,8 +206,10 @@ class CalendarEvent(models.Model):
 
                 for day in event_days:
 
-                    datetime_start = datetime.combine(day, time.min, event_start.tzinfo)
-                    datetime_end = datetime.combine(day, time.max, event_stop.tzinfo)
+                    datetime_start = datetime.combine(day, time.min)
+                    datetime_start.tzinfo = event_start.tzinfo
+                    datetime_end = datetime.combine(day, time.max)
+                    datetime_end.tzinfo =  event_stop.tzinfo
 
                     intervals = \
                         resource.calendar_id._attendance_intervals(
