a9a4e6c9cf67851910111954030d48ff4a2b61e8
César López Ramírez <cesar.lopez@coopdevs.org>
2021-02-19 16:47:10 +0100

    Initial commit

---
+++ calendar_schedulable/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Calendar Schedulable',
+    'version': '12.0.0.0.1',
+    'category': 'Calendar',
+    'summary': 'Mixin to allow scheduling for a model',
+    'description': "",
+    'website': 'https://github.com/oca/calendar',
+    'depends': ['base'],
+    'data': [
+    ],
+    'installable': True,
+    'application': True,
+}
+
---
+++ calendar_schedulable/models/calendar_schedulable.py
@@ -0,0 +1,6 @@
+from odoo import models
+
+
+class CalendarSchedulable(models.AbstractModel):
+    pass
+
