b8242ddb8eb0a3ebbcf6463c3350311011fb8891
Kapis <kapis@riseup.net>
2021-03-08 13:18:14 +0100

    Empezando modulo que hereda del mixin schedulable

---
+++ calendar_project_task_schedulable/__manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+{
+    "name": "Calendar Project Task Schedulable",
+    "version": "12.0.0.0.1",
+    "category": "Calendar",
+    "summary": "Module to allow scheduling for a project task",
+    "description": "",
+    "website": "https://github.com/oca/calendar",
+    "depends": ["hr", "calendar_schedulable"],
+    "data": [],
+    "installable": True,
+    "application": False,
+}
