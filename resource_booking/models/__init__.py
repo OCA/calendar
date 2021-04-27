551ac7aebe479c1203da1f45714887419438f756
Jairo Llopis <jairo.llopis@tecnativa.com>
2021-04-27 10:34:02 +0100

    [ADD] resource_booking: new app to manage bookings This module adds a new app to allow you to book resource combinations in given schedules.
    Example use cases:
    
    * Management of consultations in a clinic.
    * Salesman appointments.
    * Classroom and projector reservations.
    * Hotel room booking.
    
    Among the things you can do:
    
    * Specify the type of booking, which includes a calendar of availability.
    * Specify which resources can be booked together. All of them must be free to be booked.
    * Place pending bookings, effectively giving permissions to someone to see the availability calendar and choose one slot.
    * Partners can do that from their portals.
    * If a partner has no user, he can still do the same via a tokenized URL.
    * Backend users can also do that from the backend.
    * Booking lifecycle with computed states.
    * Automatic meeting creation and deletion.
    * Automatic conflict detection.
    * Deadline to block modifications.
    
    @Tecnativa TT28201

---
+++ resource_booking/models/__init__.py
@@ -0,0 +1,7 @@
+from . import calendar_event
+from . import resource_booking
+from . import resource_booking_combination
+from . import resource_booking_type
+from . import resource_booking_type_combination_rel
+from . import resource_calendar
+from . import resource_resource
