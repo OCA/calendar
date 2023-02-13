120557cde7d9c574c92116500b1bb2fdc2d42b96
hugues de keyzer <odoo@hugues.info>
2023-02-14 00:23:02 +0100

    [ADD] add calendar_resource_booking_type_color
    add new module calendar_resource_booking_type_color.

---
+++ calendar_resource_booking_type_color/__manifest__.py
@@ -0,0 +1,19 @@
+# SPDX-FileCopyrightText: 2023 Coop IT Easy SC
+#
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
+{
+    "name": "Calendar Resource Booking Type Color",
+    "summary": "Make calendar events color depend on the booking type",
+    "version": "14.0.1.0.0",
+    "category": "Appointments",
+    "website": "https://github.com/OCA/calendar",
+    "author": "Coop IT Easy SC, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "depends": [
+        "resource_booking",
+    ],
+    "data": [
+        "views/resource_booking_view.xml",
+    ],
+}
---
+++ calendar_resource_booking_type_color/readme/DESCRIPTION.rst
@@ -0,0 +1,2 @@
+This module makes the resource booking calendar events color depend on the
+booking type.
