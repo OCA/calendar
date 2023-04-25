d609557a7d59a5287a1d446f928e02ad734e4a87
Víctor Martínez <victor.martinez@tecnativa.com>
2023-04-25 17:08:45 +0200

    [IMP] resource_booking: Partial change from tour test to js

--- resource_booking/__manifest__.py
+++ resource_booking/__manifest__.py
@@ -40,6 +40,7 @@
     ],
     "assets": {
         "web.assets_frontend": ["resource_booking/static/src/scss/portal.scss"],
+        "web.assets_tests": ["resource_booking/static/src/js/resource_booking_tour.js"],
     },
     "demo": ["demo/res_users_demo.xml"],
 }
