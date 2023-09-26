16e94ba49910addc056d071f08faa9f5d2186b39
Víctor Martínez <victor.martinez@tecnativa.com>
2023-09-26 16:17:04 +0200

    [FIX] resource_booking: Add class to schedule page to show full dropdown if opened at the bottom
    TT45274
    
    [BOT] post-merge updates

--- resource_booking/__manifest__.py
+++ resource_booking/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Resource booking",
     "summary": "Manage appointments and resource booking",
-    "version": "15.0.1.1.1",
+    "version": "15.0.1.2.0",
     "development_status": "Production/Stable",
     "category": "Appointments",
     "website": "https://github.com/OCA/calendar",
