c340742f4775412637ae849bd530866517ab63cc
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2023-11-29 15:57:50 +0100

    [OU-FIX] resource_booking: Proper place for the script

---
+++ resource_booking/migrations/15.0.1.4.0/post-migration.py
@@ -0,0 +1,19 @@
+# Copyright 2023 Tecnativa - Carolina Fernandez
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+from openupgradelib import openupgrade
+
+
+def convert_resource_booking_partners(env):
+    openupgrade.m2o_to_x2x(
+        env.cr,
+        env["resource.booking"],
+        "resource_booking",
+        "partner_ids",
+        "old_partner_id",
+    )
+
+
+@openupgrade.migrate()
+def migrate(env, version):
+    """Put partner_id in partner_ids"""
+    convert_resource_booking_partners(env)
