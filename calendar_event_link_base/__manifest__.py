8161f28a3a265a4a22fb917a8f1dd4078431b3f5
Cyril VINH-TUNG <cyril@invitu.com>
2024-01-22 08:46:07 -1000

    [MIG] calendar_event_link_base: Migration to 16.0

--- calendar_event_link_base/__manifest__.py
+++ calendar_event_link_base/__manifest__.py
@@ -6,7 +6,7 @@
     "summary": """
         This module add an abstract model that provide an action to link
         any model to calendar events""",
-    "version": "12.0.1.0.0",
+    "version": "16.0.1.0.0",
     "license": "AGPL-3",
     "author": "ACSONE SA/NV," "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/calendar",
