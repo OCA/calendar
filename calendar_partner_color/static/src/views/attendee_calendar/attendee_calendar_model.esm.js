08229d8893d81908debc338c23c4a68e5822c00b
LorenzoC0 <lorenzo.carta@innovyou.it>
2025-03-05 13:07:58 +0100

    [FIX] calendar_partner_color: Fixed fetching of private events

--- calendar_partner_color/static/src/views/attendee_calendar/attendee_calendar_model.esm.js
+++ calendar_partner_color/static/src/views/attendee_calendar/attendee_calendar_model.esm.js
@@ -23,12 +23,22 @@ patch(AttendeeCalendarModel.prototype, "calendar_partner_color", {
                 .set(attendee.attendee_id, attendee.color);
         }
 
+        const missingColors = new Set();
         // Assign colorIndex based on matching attendee
         for (const event of Object.values(data.records)) {
             if (attendeeMap.has(event.id)) {
                 const eventAttendees = attendeeMap.get(event.id);
                 if (eventAttendees.has(event.calendarAttendeeId)) {
                     event.colorIndex = eventAttendees.get(event.calendarAttendeeId);
+                }
+            } else if (event.rawRecord.privacy === "private") {
+                if (!missingColors.has(event.rawRecord.partner_id[0])) {
+                    event.colorIndex = await this.getColorPrivateEvent(
+                        event.rawRecord.partner_id[0],
+                        missingColors
+                    );
+                } else {
+                    event.colorIndex = missingColors.get(event.rawRecord.partner_id[0]);
                 }
             }
         }
@@ -48,5 +58,18 @@ patch(AttendeeCalendarModel.prototype, "calendar_partner_color", {
             }
         }
         return res;
+    },
+
+    async getColorPrivateEvent(partner_id, missingColors) {
+        const colorIndex = await this.orm.searchRead(
+            "res.partner",
+            [["id", "=", partner_id]],
+            ["color"]
+        );
+        if (colorIndex.length) {
+            missingColors.add(partner_id[0], colorIndex[0].color);
+            return colorIndex[0].color;
+        }
+        return null; // Return a default value if no color is found
     },
 });
